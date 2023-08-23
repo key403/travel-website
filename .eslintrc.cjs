0c3a1d1c15914d409fbafbdbb31e9ac84658fdda
key403 <m.saenzz2311@gmail.com>
2023-08-22 20:54:31 -0400

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "next",
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
   },
 }
